91046d505d832e77e29290c9a22ba8ef8ce19917
aldoalyasin380-star <aldoalyasin380@gmail.com>
2026-09-21 14:32:59 +0700

    Add ytplay downloader API endpoint

---
+++ src/api/downloader/ytplay.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+    app.get('/downloader/ytplay', async (req, res) => {
+        try {
+            const queryParams = new URLSearchParams(req.query).toString();
+            const targetUrl = `https://api.nexray.eu.cc/downloader/ytplay${queryParams ? '?' + queryParams : ''}`;
+            
+            const response = await axios.get(targetUrl);
+            return res.status(200).json(response.data);
+        } catch (error) {
+            console.error("Error fetching Ytplay:", error.message);
+            return res.status(500).json({ status: false, error: 'Gagal mengambil data dari API eksternal' });
+        }
+    });
+};
